b7e17ec58987801a27ce48b2fc33d433e3d28c93
whatisRT <andre.knispel@gmx.de>
2023-11-08 21:54:32 +0100

    Add proper exit code to tests

--- Test/Test.hs
+++ Test/Test.hs
@@ -2,12 +2,16 @@ module Main where
 
 import LAM.Test
 
+import System.Exit
 import Test.QuickCheck
 
-doTest t = quickCheck (withMaxSuccess 10000 t)
+doTest t = isSuccess <$> quickCheckResult (withMaxSuccess 10000 t)
 
 main :: IO ()
 main = do
-  doTest roundtripToDB
-  doTest roundtripToDBT0
-  doTest roundtripToDBT1
+  res <- and <$> sequence
+    [ doTest roundtripToDB
+    , doTest roundtripToDBT0
+    , doTest roundtripToDBT1
+    ]
+  if res then exitSuccess else exitFailure
