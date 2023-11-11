06cf079a19908af1023089ffb8f847e6d220b790
whatisRT <andre.knispel@gmx.de>
2023-11-11 16:17:19 +0100

    Add `runTraceFinite` for use in tests

--- Test/LAM/Exec/Test.hs
+++ Test/LAM/Exec/Test.hs
@@ -7,8 +7,8 @@ import LAM.Pure
 compareLAMs :: (ToPureState s1 NamedList, ToPureState s2 NamedList)
   => IsLAM IO e1 s1 t -> IsLAM IO e2 s2 t -> t -> IO Bool
 compareLAMs l l' t = do
-  trace1 <- traverse toPureState =<< runTrace l  t :: IO [PState NamedList]
-  trace2 <- traverse toPureState =<< runTrace l' t :: IO [PState NamedList]
+  trace1 <- traverse toPureState =<< runTraceFinite 1000 l  t :: IO [PState NamedList]
+  trace2 <- traverse toPureState =<< runTraceFinite 1000 l' t :: IO [PState NamedList]
   if length trace1 /= length trace2 then return False
     else do comps <- sequence $ zipWith heuristicCompState trace1 trace2
             return $ foldr (&&) True comps
