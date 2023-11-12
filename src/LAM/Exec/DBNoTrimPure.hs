a20bc1c8fe91d031293962050a9629306b99713e
whatisRT <andre.knispel@gmx.de>
2023-11-12 15:46:50 +0100

    Add `stateToTerm` function and use it in some places

--- src/LAM/Exec/DBNoTrimPure.hs
+++ src/LAM/Exec/DBNoTrimPure.hs
@@ -1,22 +1,56 @@
 module LAM.Exec.DBNoTrimPure where
 
 import LAM.Base hiding (State)
+import LAM.CanTrim
 import LAM.Pure
 import LAM.IsDBEnv
 import LAM.IsLAM
+import Trie.Map (Trie)
 
+import Data.Foldable
 import Data.IORef
 
 import qualified Trie.Map as Trie
 import Data.Sequence (Seq)
+import qualified Data.Sequence as Seq
 import Data.Vector (Vector)
+import qualified Data.Vector as Vec
 
 type HeapPointer t = RHeapPointer IORef DBTerm t
 type State t = RState IORef DBTerm t
 
 type Err = String
 
+instance IsNamedEnv Seq where
+  eToList s = eToList $ toList s
+  eFromList l = Seq.fromList $ eFromList l
+
+instance IsNamedEnv Vector where
+  eToList s = eToList $ toList s
+  eFromList l = Vec.fromList $ eFromList l
+
+instance CanTrim DBTerm [] where
+  trim c = c
+
+instance CanTrim DBTerm Seq where
+  trim c = c
+
+instance CanTrim DBTerm Vector where
+  trim c = c
+
 instance ToPureState (State NamedList) NamedList where
+  toPureState = toPureStateGen id
+
+instance ToPureState (State NamedList) Trie where
+  toPureState = toPureStateGen id
+
+instance ToPureState (State []) Trie where
+  toPureState = toPureStateGen id
+
+instance ToPureState (State Seq) Trie where
+  toPureState = toPureStateGen id
+
+instance ToPureState (State Vector) Trie where
   toPureState = toPureStateGen id
 
 mark2 :: IsDBEnv t => State t -> IO (Either Err (State t))
